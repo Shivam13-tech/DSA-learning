13e527866c04796dfec727f0b882cfe5f00d3a04
Shivam <shivampandey88@hotmail.com>
2023-04-08 15:17:33 +0530

    mergesort part 2

--- Merge sort/merge.py
+++ Merge sort/merge.py
@@ -17,4 +17,12 @@ def merge(list1,list2):
         j += 1
     return combined
 
-print(merge([1,2,7,8],[3,4,5,6]))
+def merge_sort(my_list):
+    if len(my_list) == 1:
+        return my_list
+    mid = int(len(my_list)/2)
+    left = my_list[:mid]
+    right = my_list[mid:]
+    return merge(merge_sort(left),merge_sort(right))
+
+print(merge_sort([3,1,4,2]))
